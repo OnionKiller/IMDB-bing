af34ae0d20b5fbec53ce01e5d5951622ec84d9d7
OnionKiller <tomi@csilling.com>
2021-01-28 00:00:48 +0100

    move functions to frontend

--- foreground.js
+++ foreground.js
@@ -1,20 +1,47 @@
-function render() {
-    const mock_mins = 2581;
+function render(mins = 2650 /* mock value*/) {
 
     const box = document.querySelector(".title_wrapper .subtext");
+    if (box === null)
+        return;
     const separator = document.querySelector(".title_wrapper .subtext .ghost");
 
-    box.appendChild(separator.cloneNode(true));
-    box.appendChild(box.children[0].cloneNode(true));
+    const point_of_interest = document.querySelectorAll(".title_wrapper .subtext .ghost")[0];
+    box.insertBefore(separator.cloneNode(true),point_of_interest);
+    box.insertBefore(box.children[0].cloneNode(true),point_of_interest);
 
-    const time = box.lastChild;
-    time.dateTime = "PT" + mock_mins + "M";
-    time.innerText = "" + (mock_mins / 60).toFixed(2) + "h";
-
-    console.log("frontend hello");
+    const time = document.querySelectorAll(".title_wrapper .subtext time")[1];;
+    time.dateTime = "PT" + mins + "M";
+    time.innerText = "~" + (mins / 60).toFixed(1) + "hour full";
 }
+
+async function get_api_keys() {
+    return new Promise((resolve, reject) => {
+        chrome.runtime.sendMessage({ get: "api" }, response => {
+            resolve(response);
+        });
+    });
+}
+
+//set reciving end
 chrome.runtime.onMessage.addListener((request, sender, senResponse) => {
     console.log(request);
 });
+
 console.log("frontend fully loaded");
-render();
+get_api_keys().then(r => {
+    //console.log(r);
+    var tmdb_api_key = r.keys.tmdb;
+    estimate_full_size(get_imdb_tt_id(window.location.href), tmdb_api_key).then(r => {
+        render(r);
+    })
+});
+
+/* investigate why this doesn't work
+TODO
+chrome.runtime.sendMessage({
+    get:"estimate",
+    tt:get_imdb_tt_id(window.location.href)
+},response=>{
+    console.log(response);
+    //render(response.min);
+})*/
